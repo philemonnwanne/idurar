e99fd6f9689ea397697145b3aac18b7ea04c7737
philemonnwanne <philemonnwanne@gmail.com>
2023-08-23 10:22:15 +0100

    update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -4,11 +4,11 @@ const GITPOD_WORKSPACE_CLUSTER_HOST = process.env.REACT_APP_GITPOD_WORKSPACE_CLU
 export const API_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
     ? 'https://dev-server.idurarapp.com/api/'
-    // ? `https://8888-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}/api/`
+    // ? `https://8888-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}/api/` (disable 6 then swap 7&8 to work on Gitpod)
     : 'http://localhost:8888/api/';
 export const DOWNLOAD_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
     ? 'https://dev-server.idurarapp.com/download/'
-    // ? `https://8888-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}/download/`
+    // ? `https://8888-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}/download/` (disable 6 then swap 7&8 to work on Gitpod)
     : 'http://localhost:8888/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
